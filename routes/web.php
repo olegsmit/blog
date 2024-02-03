514ce3b608eaef25302f3d88ed076f36e0b307d0
osmit <oleg.smit@gmail.com>
2024-02-03 23:36:15 +0200

    mod 6

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::group(['namespace'=> 'App\Http\Controllers\Admin', 'prefix'=>'admin'], fu
     });
 
     Route::group(['namespace' => 'Category', 'prefix'=>'categories'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
     });
 });
 Auth::routes();
