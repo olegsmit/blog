227a096e42248db71077e0bdfce9b0beca272c34
osmit <oleg.smit@gmail.com>
2024-02-04 02:13:47 +0200

    mod 12

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Додавання категорії</h1>
+                        <h1 class="m-0">Редагування категорії</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -26,15 +26,17 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="{{route('admin.category.store')}}" method="POST" class="w-25">
+                        <form action="{{route('admin.category.update', $category->id) }}" method="POST" class="w-25">
                             @csrf
+                            @method('PATCH')
                             <div class="form-group">
-                                <input type="text" class="form-control" name="title" placeholder="Назва категорії">
+                                <input type="text" class="form-control" name="title" placeholder="Назва категорії"
+                                       value="{{$category->title}}">
                                 @error('title')
                                 <div class="text-danger">Це поле не може бути пустим.</div>
                                 @enderror
                             </div>
-                            <input type="submit" class="btn btn-info" value="Додати">
+                            <input type="submit" class="btn btn-info" value="Оновити">
                         </form>
                     </div>
                 </div>
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Категорії</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -25,11 +25,48 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-1">
+                    <div class="col-1 mb-3">
                         <a href="{{route('admin.category.create')}}" class="btn btn-block btn-info">Додати</a>
                     </div>
-                    <div class="col-12">
-                        Категорії
+                </div>
+                <div class="row">
+                    <div class="col-8">
+
+                        <div class="card">
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Назва</th>
+                                        <th colspan="3" class="text-center">Дія</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{$category->id}}</td>
+                                            <td>{{$category->title}}</td>
+                                            <td class="text-center"><a href="{{route('admin.category.show', $category->id)}}"
+                                                   class="text-info"><i class="far fa-eye"></i></a></td>
+                                            <td class="text-center"><a href="{{route('admin.category.edit', $category->id)}}"
+                                                   class="text-info"><i class="fas fa-pencil-alt"></i></a></td>
+                                            <td class="text-center">
+                                                <form action="{{route('admin.category.delete', $category->id)}}"method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit"class="border-0 bg-transparent">
+                                                    <i class="fas fa-trash text-danger" role="button"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
